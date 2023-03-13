38d47f8762b8a1fd2e8ebad50b911b1886f05f1b
Thobo <gaboipewe.thobo@gmail.com>
2023-03-13 14:33:07 +0200

    Initial mobile display markup

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello World!')
+const button = document.querySelector("#button")
+// const firstName = document.querySelector("")
+
+
+button.addEventListener('click', () => {
+    console.log('The button is working.');
+})
+
+
+// check if input is empty
+function isEmpty(input) {
+
+
+    if (input == "") {
+
+    }   
+}
+
+
+// insert alertText
